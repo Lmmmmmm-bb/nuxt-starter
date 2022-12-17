7d7e2732164c91f0e3c30cf4989d5ac8a57040ce
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-12-17 21:10:13 +0800

    feat: uno support

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div font-serif h-full flex items-center justify-center>
+    Hello Nuxt
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<style>
+html, body , #__nuxt{
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
