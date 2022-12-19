34a4e96c5b0638217bfa77c056b6de0aa2b5fe15
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-12-20 00:23:49 +0800

    feat: color mode support

---
+++ components/theme-toggle.vue
@@ -0,0 +1,13 @@
+<script setup>
+const colorMode = useColorMode();
+
+const toggleDark = () => {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+};
+</script>
+
+<template>
+  <button @click="toggleDark">
+    <div class="dark:i-carbon-moon i-carbon-sun" />
+  </button>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@unocss/nuxt'],
+  modules: ['@unocss/nuxt', '@nuxtjs/color-mode'],
   css: [
     '~/index.css',
     '@unocss/reset/tailwind.css'
-  ]
+  ],
+  colorMode: { classSuffix: '' }
 });
