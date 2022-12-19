1f73dda38a8cbd4bf741b026b7fa30d878e432a7
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-12-20 00:23:49 +0800

    feat: color mode support

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
 <template>
-  <div font-serif h-full flex items-center justify-center>
-    Hello Nuxt
+  <div
+    font-serif
+    h-full
+    flex
+    flex-col
+    items-center
+    justify-center
+  >
+    <h3>Hello Nuxt</h3>
+    <ThemeToggle />
     <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<style>
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
