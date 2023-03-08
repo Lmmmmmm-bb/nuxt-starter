4908da2d83acaab443a95826f1264124595f0d67
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-03-09 00:00:26 +0800

    chore: components dirs

--- components/ThemeToggle/index.vue
+++ components/ThemeToggle/index.vue
@@ -4,12 +4,10 @@ const colorMode = useColorMode();
 const toggleDark = () => {
   colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
 };
-
-const icon = computed(() => colorMode.value === 'dark' ? 'carbon:moon' : 'carbon:sun');
 </script>
 
 <template>
   <button @click="toggleDark">
-    <Icon :name="icon" />
+    <Icon :name="colorMode.value === 'dark' ? 'carbon:moon' : 'carbon:sun'" />
   </button>
 </template>
