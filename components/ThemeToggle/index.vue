60b24b562621537baf26c606f1420b95def911f2
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-11-11 21:52:16 +0800

    style(theme): transition api support

--- components/ThemeToggle/index.vue
+++ components/ThemeToggle/index.vue
@@ -12,8 +12,39 @@ useHead({
   ],
 });
 
-const toggleDark = () => {
-  colorMode.preference = isDarkMode.value ? 'light' : 'dark';
+const toggleDark = async (e: MouseEvent) => {
+  // @ts-expect-error experimental API
+  const isAppearanceTransition = document.startViewTransition
+    && !window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
+  if (!isAppearanceTransition) {
+    colorMode.preference = isDarkMode.value ? 'light' : 'dark';
+    return;
+  }
+
+  const [x, y] = [e.clientX, e.clientY];
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  );
+  // @ts-expect-error: Transition API
+  const transition = document.startViewTransition(async () => {
+    colorMode.preference = isDarkMode.value ? 'light' : 'dark';
+    await nextTick();
+  });
+  await transition.ready;
+  const clipPath = [
+    `circle(0px at ${x}px ${y}px)`,
+    `circle(${endRadius}px at ${x}px ${y}px)`,
+  ];
+  document.documentElement.animate(
+    { clipPath },
+    {
+      duration: 400,
+      easing: 'ease-out',
+      pseudoElement: '::view-transition-new(root)',
+    },
+  );
 };
 </script>
 
